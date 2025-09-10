00c6cda5e7307cf8a3397768e1a3284cc5f475a4
GunjalArpita <gunjalarpita80@gmail.com>
2025-09-10 23:50:48 +0530

    Super Admin Dashboard

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import SuperAdminRegister from './SuperAdmin/SuperAdminRegister'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SuperAdminRegister from './SuperAdmin/SuperAdminRegister'
 import SuperAdminLogin from './SuperAdmin/SuperAdminLogin';
+import SuperAdminDashboard from './SuperAdmin/SuperAdminDashboard';
+
 
 function App() {
   return (
     <>
     <SuperAdminRegister/>
     <SuperAdminLogin></SuperAdminLogin>
+    <SuperAdminDashboard/>
     </>
   )
 }
